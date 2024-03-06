8a05ea1589934a298257ffecc12868616479a845
Brian Chebon <brianlchebon@gmail.com>
2024-03-06 12:54:04 +0300

    added the indvidual listing

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,7 +1,7 @@
 @props(['listing'])
 
 
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<x-card>
                     <div class="flex">
                         <img
                             class="hidden w-48 mr-6 md:block"
@@ -13,31 +13,10 @@
                                 <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
                             </h3>
                             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-                            <ul class="flex">
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Laravel</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">API</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Backend</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Vue</a>
-                                </li>
-                            </ul>
+                            <x-listing-tags :tagsCsv="$listing->tags" />
                             <div class="text-lg mt-4">
                                 <i class="fa-solid fa-location-dot"></i>{{$listing->loaction}}
                             </div>
                         </div>
                     </div>
-                </div>
+</x-card>
