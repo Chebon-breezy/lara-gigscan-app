b0395def280c5d0a5eca9200f61fc7bc4f6da52c
Brian Chebon <brianlchebon@gmail.com>
2024-03-07 11:59:35 +0300

    sorted the tags using scopeFilter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,8 +9,9 @@ class ListingController extends Controller
 {
     //show all listings
     public function index(){
+        // dd(request('tag'));
         return view('listings.index', [
-            'listings' => Listings::all(),
+            'listings' => Listings::latest()->filter(request(['tag']))->get(),
         ]);
 
     }
